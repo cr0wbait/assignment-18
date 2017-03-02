aad5a249b098d75d9121555872684d4045dd66fc
cr0wbait <etcothran@gmail.com>
2017-03-02 09:27:17 -0500

    hacking at js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 console.log('wat')
 
-$.getJSON('http://docs.apis.is/').then((serverRes)=>{
-  console.log(serverRes)
-
-})
+let talkaboutInboundFlights = $.getJSON('http://apis.is/flight?language=en&type=arrivals')
+let talkaboutOutboundFlights = $.getJSON('http://apis.is/flight?language=en&type=departures')
+let navOps = document.querySelector('.navop')
+function renderContentTo(domEl, theRoute){
+  let allThangs = ''
+  if (theRoute === 'home')
+}
